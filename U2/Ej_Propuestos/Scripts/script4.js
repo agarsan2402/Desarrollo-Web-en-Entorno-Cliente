3a151df925ebb657fc8fd9ade819a909f13e4e5e
agarsan2402 <agarsan2402@g.educaand.es>
2024-10-23 12:11:56 +0200

    commit

--- U2/Ej_Propuestos/Scripts/script4.js
+++ U2/Ej_Propuestos/Scripts/script4.js
@@ -1,11 +1,10 @@
-var boton = document.getElementById('calcular');
-boton.addEventListener("click", calculo, false);
+var boton = document.getElementById('tipos');
+boton.addEventListener("click", tipos, false);
 
-function calculo() 
+function tipos() 
 {
-    var radio = document.getElementById('radio').value;
-
-    document.getElementById('area').innerHTML = Math.PI * radio + " area";
-    document.getElementById('longitud').innerHTML = 2 * Math.PI * radio + " longitud";
-    
+    var tipo1 = tipo1=parseInt(document.getElementById('e').value);
+    tipo1=typeof(tipo1);
+    var tipo2 = typeof(document.getElementById('c').value);
+    document.getElementById('res').innerHTML = "Tipo 1: "+tipo1+"<br>Tipo 2: "+tipo2;   
 }
