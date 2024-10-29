88f8b37ffe3d40e052c97a85bcf6fb85befee90a
agarsan2402 <agarsan2402@g.educaand.es>
2024-10-29 21:05:06 +0100

    commit

--- U2/Act_Evaluable/script.js
+++ U2/Act_Evaluable/script.js
@@ -1,4 +1,29 @@
+var cad="";
 for (let i = 1; i <= 10; i++) {
-    document.getElementById('res').innerHTML = 7 * i;
-    
+    cad+="7 * "+i+" = "+7*i+"<br>";
 }
+document.getElementById('t7').innerHTML = cad;
+
+cad="";
+var i=1;
+do {
+    cad+="8 + "+i+" = "+(8+i)+"<br>";
+    i++;
+} while (i<=10);
+document.getElementById('t8').innerHTML = cad;
+
+cad="";
+var i=1;
+do {
+    cad+="8 + "+i+" = "+(8+i)+"<br>";
+    i++;
+} while (i<=10);
+document.getElementById('t8').innerHTML = cad;
+
+cad="";
+var j=1;
+while (j<=10){
+    cad+="9 / "+j+" = "+(9/j).toFixed(1)+"<br>";
+    j++;
+} 
+document.getElementById('t9').innerHTML = cad;
