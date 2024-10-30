b9127f988421c94327d1d6fac012fd8e39d28b7b
agarsan2402 <agarsan2402@g.educaand.es>
2024-10-30 09:01:20 +0100

    commit

--- U2/Act_Evaluable/script.js
+++
@@ -1,4 +0,0 @@
-for (let i = 1; i <= 10; i++) {
-    document.getElementById('res').innerHTML = 7 * i;
-    
-}
---
+++ U2/TareaEvaluable_RA2-bfg-Tablas de Multiplicar/script.js
@@ -0,0 +1,35 @@
+var cad="";
+for (let i = 1; i <= 10; i++) {
+    cad+="7 * "+i+" = "+7*i+"<br>";
+}
+document.getElementById('t7').innerHTML = cad;
+
+cad="";
+var i=1;
+do {
+    cad+="8 + "+i+" = "+(8+i)+"<br>";
+    i++;
+} while (i<=10);
+document.getElementById('t8').innerHTML = cad;
+
+cad="";
+var i=1;
+do {
+    cad+="8 + "+i+" = "+(8+i)+"<br>";
+    i++;
+} while (i<=10);
+document.getElementById('t8').innerHTML = cad;
+
+cad="";
+var j=1;
+while (j<=10){
+    cad+="9 / "+j+" = "+(9/j).toFixed(1)+"<br>";
+    j++;
+} 
+document.getElementById('t9').innerHTML = cad;
+
+function desplazamiento_Drcha() {
+    
+}
+document.write((125>>3));
+document.write((625<<3));
